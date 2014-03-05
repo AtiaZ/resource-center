2e775d5ec95e361513f5a98752fd738d744cc59b
Dao Cong Anh <anhdc@exoplatform.com>
2014-03-05 12:05:07 +0700

    PLFENG-2413: clean up NoteUI source code

--- Vaadin/QuickNote/src/com/example/quicknote/NoteUI.java
+++ Vaadin/QuickNote/src/com/example/quicknote/NoteUI.java
@@ -3,16 +3,13 @@ package com.example.quicknote;
 import java.io.File;
 import java.io.IOException;
 
-import javax.servlet.annotation.WebServlet;
-
+import com.google.gwt.thirdparty.guava.common.io.Files;
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.data.util.TextFileProperty;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -31,115 +28,93 @@ import com.vaadin.ui.Window;
 @Theme("runo")
 public class NoteUI extends UI {
 
-	@WebServlet(value = "/*", asyncSupported = true)
-	@VaadinServletConfiguration(productionMode = true, ui = NoteUI.class)
-	public static class Servlet extends VaadinServlet {
-	}
+	public static File NOTES_DIR = Files.createTempDir();
+	// To use a persistent directory in exo data dir, use this:
+	// public static File NOTES_DIR = new File("data/quicknote");
 	
-	FilesystemContainer notes = new FilesystemContainer(new File("data/quicknote"));
+	FilesystemContainer notes = new FilesystemContainer(NOTES_DIR);
 	Table noteList = new Table("Notes", notes);
-  RichTextArea noteView = new RichTextArea();
-  Button add = new Button("+"); 
-  String filename;
-  Window subWindow = new Window("Enter note name:");
-  TextField txtname = new TextField();
-  Button submit = new Button("OK");
-  Button cancel = new Button("Cancel");
-	
+	RichTextArea noteView = new RichTextArea();
+	Button add = new Button("+");
+	String filename;
+	Window subWindow = new Window("Enter note name:");
+	TextField txtname = new TextField();
+	Button ok = new Button("OK");
+	Button cancel = new Button("Cancel");
+
 	@Override
 	protected void init(VaadinRequest request) {
-	  
-	  VerticalSplitPanel split = new VerticalSplitPanel();
-	  
-	  VerticalLayout vert = new VerticalLayout();
-	  
-	  vert.addComponent(split);	  
-	  vert.addComponent(add);
-	  vert.setComponentAlignment(add, Alignment.MIDDLE_CENTER);
-	  
-	  split.addComponent(noteList);
-	  split.addComponent(noteView);
-	  split.setWidth("285px");
-	  split.setHeight("300px");
-	  
-	  noteView.setSizeFull();
-	  noteList.setSizeFull();
-	  
-	  tableRefresh(notes);
-	  
-	  noteList.addValueChangeListener(new ValueChangeListener() {      
-      public void valueChange(ValueChangeEvent event) {
-        noteView.setPropertyDataSource(new TextFileProperty((File) event.getProperty().getValue()));        
-      }
-    });
-	  
-	  //Dialog box to enter note name
-	  HorizontalLayout subHor = new HorizontalLayout();
-    VerticalLayout subVert = new VerticalLayout();
-    subVert.setMargin(true);
-    
-    subWindow.setContent(subVert);
-    // Put some components in it
-    subVert.addComponent(txtname);
-    subVert.addComponent(subHor);
-    
-    subHor.addComponent(submit);
-    subHor.addComponent(cancel);
-        
-    // Center it in the browser window
-    subWindow.setResizable(false);
-    subWindow.center();
-    
-    //Buttons click listener
-    submit.addClickListener(new ClickListener() {
-      public void buttonClick(ClickEvent event) {
-        filename = txtname.getValue();
-        if (!filename.equals("")) {
-          File newNode = new File("data/quicknote/"+filename+".txt");
-          
-          try {
-            newNode.createNewFile();          
-                      
-          } catch (IOException e) {
-            e.printStackTrace();
-          }         
-          tableRefresh(notes);
-          for (Object itemId : noteList.getItemIds()){
-            if(itemId.toString().equals(newNode.getPath())){
-              noteList.select(itemId);
-              break;
-            }
-          }
-        }
-        
-        
-        subWindow.close();        
-      }
-    });
-    
-    cancel.addClickListener(new ClickListener() {            
-      public void buttonClick(ClickEvent event) {
-        subWindow.close();        
-      }
-    });
-	  
-	  add.addClickListener(new ClickListener() {      
-      public void buttonClick(ClickEvent event) {   
-        addWindow(subWindow);        
-      }
-    });
-	  
-	  //set main layout
-	  setContent(vert);
-	  
+
+		VerticalSplitPanel split = new VerticalSplitPanel(noteList, noteView);
+		split.setHeight("300px");
+
+		noteView.setSizeFull();
+		noteList.setSizeFull();
+
+		tableRefresh(notes);
+
+		noteList.addValueChangeListener(new ValueChangeListener() {
+			public void valueChange(ValueChangeEvent event) {
+				noteView.setPropertyDataSource(new TextFileProperty(
+						(File) event.getProperty().getValue()));
+			}
+		});
+
+		// Dialog box to enter note name
+		VerticalLayout subVert = new VerticalLayout();
+		subVert.setMargin(true);
+		subVert.setSpacing(true);
+		subVert.addComponents(txtname, new HorizontalLayout(ok, cancel));
+		subWindow.setContent(subVert);
+
+		// Center it in the browser window
+		subWindow.setResizable(false);
+		subWindow.center();
+
+		// Buttons click listener
+		ok.addClickListener(new ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				filename = txtname.getValue();
+				if (!filename.equals("")) {
+					File newNode = new File(NOTES_DIR, filename);
+					try {
+						newNode.createNewFile();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					tableRefresh(notes);
+					noteList.select(newNode);
+				}
+				subWindow.close();
+			}
+		});
+
+		cancel.addClickListener(new ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				subWindow.close();
+			}
+		});
+
+		add.addClickListener(new ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				addWindow(subWindow);
+			}
+		});
+
+		// set main layout
+		VerticalLayout vert = new VerticalLayout(split, add);
+		vert.setWidth("285px");
+		vert.setComponentAlignment(add, Alignment.MIDDLE_CENTER);
+		setContent(vert);
+
 	}
-	
-	private void tableRefresh(FilesystemContainer data){
-	  noteList.setContainerDataSource(data);
-	  noteList.setSelectable(true);
-    noteList.setImmediate(true);    
-    noteList.setColumnHeaderMode(ColumnHeaderMode.HIDDEN);
-    noteList.setVisibleColumns(FilesystemContainer.PROPERTY_NAME);
+
+	private void tableRefresh(FilesystemContainer data) {
+		noteList.setContainerDataSource(data);
+		noteList.setSelectable(true);
+		noteList.setImmediate(true);
+		noteList.setColumnHeaderMode(ColumnHeaderMode.HIDDEN);
+		noteList.setVisibleColumns(FilesystemContainer.PROPERTY_NAME);
 	}
 
 }
